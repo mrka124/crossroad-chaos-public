5cb006e482dde89f0f8a5f8e7f5460f73b31390e
kiet.dinh <dinhvankiet124@gmail.com>
2026-06-21 09:18:29 +0700

    chore: remove stale image import metadata and update web build cache version

--- builds/web/crossroad-chaos.service.worker.js
+++ builds/web/crossroad-chaos.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781947353|2340408272';
+const CACHE_VERSION = '1782008237|1829894';
 /** @type {string} */
 const CACHE_PREFIX = 'Crossroad Chaos-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
