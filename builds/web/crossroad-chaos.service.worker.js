1256afb23ba2617af2116dee2f10442e1ec902a4
kiet.dinh <dinhvankiet124@gmail.com>
2026-06-21 10:24:04 +0700

    build: update Godot export configuration and service worker cache version

--- builds/web/crossroad-chaos.service.worker.js
+++ builds/web/crossroad-chaos.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782011190|2260046';
+const CACHE_VERSION = '1782012066|2171481';
 /** @type {string} */
 const CACHE_PREFIX = 'Crossroad Chaos-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
