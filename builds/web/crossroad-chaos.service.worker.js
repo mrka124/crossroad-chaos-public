42da1b12f64fd20d5bd8f75529b061f595f7ec23
kiet.dinh <dinhvankiet124@gmail.com>
2026-06-27 02:03:38 +0700

    build: update service worker cache version and game package size in Godot config

--- builds/web/crossroad-chaos.service.worker.js
+++ builds/web/crossroad-chaos.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782490820|2096200';
+const CACHE_VERSION = '1782500561|2636998';
 /** @type {string} */
 const CACHE_PREFIX = 'Crossroad Chaos-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
