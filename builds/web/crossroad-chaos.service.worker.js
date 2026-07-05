fd0eeb6eb72aa1ca70abd6310d1098cedcb565bf
kiet.dinh <dinhvankiet124@gmail.com>
2026-07-05 13:48:09 +0700

    build: update service worker cache version and PCK file size in Godot config

--- builds/web/crossroad-chaos.service.worker.js
+++ builds/web/crossroad-chaos.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783232731|2394973';
+const CACHE_VERSION = '1783233981|2094471';
 /** @type {string} */
 const CACHE_PREFIX = 'Crossroad Chaos-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
