f21b4a5912a60d2f67a6188783e45c8e61db3d4e
kiet.dinh <dinhvankiet124@gmail.com>
2026-07-09 23:25:00 +0700

    build: update asset files and service worker cache version

--- builds/web/crossroad-chaos.service.worker.js
+++ builds/web/crossroad-chaos.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783357415|2522144';
+const CACHE_VERSION = '1783614269|3011966';
 /** @type {string} */
 const CACHE_PREFIX = 'Crossroad Chaos-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
